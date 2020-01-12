8fd7d38411b9b30e8ebff52ebba3c7736da87c61
lj0812 <xoyljlj@163.com>
2020-01-12 23:01:48 +0800

    【更新】button 组件

---
+++ packages/button/button.vue
@@ -0,0 +1,28 @@
+<template>
+  <component :is="tagName" :class="classes" :disabled="isDisabled">
+
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+
+  },
+  computed: {
+    isLinkPattern () {
+      const { to } = this
+      return !!to
+    },
+
+    tagName () {
+      return this.isLinkPattern ? 'a' : 'button'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
