7e5f9abc2876200deb80cc6fb67df5200bcf9f9d
lj0812 <xoyljlj@163.com>
2020-01-14 18:17:22 +0800

    【新增】icon组件及文档

---
+++ packages/icon/icon.vue
@@ -0,0 +1,52 @@
+<template>
+  <i :class="classes" :style="styles"></i>
+</template>
+
+<script>
+const prefixClass = 'carbon-icon'
+
+export default {
+  name: 'Icon',
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    color: {
+      type: String,
+      default: ''
+    },
+    size: {
+      type: [Number, String],
+      default: ''
+    }
+  },
+  computed: {
+    classes () {
+      return [
+        prefixClass,
+        `${prefixClass}-${this.type}`
+      ]
+    },
+    styles () {
+      let style = {}
+
+      if (this.color) {
+        style.color = this.color
+      }
+
+      if (this.size) {
+        style['font-size'] = typeof this.size === 'number'
+          ? this.size + 'px'
+          : this.size
+      }
+
+      return style
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
